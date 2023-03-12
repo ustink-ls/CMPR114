35791ca9e9f7de54daf8c3ecb463fea6c09d9226
ustink-ls <125338425+ustink-ls@users.noreply.github.com>
2023-03-12 15:38:42 -0700

    Add files via upload

---
+++ m3 chapter 4 loops + class exercise #3 Part 1_#5.py
@@ -0,0 +1,9 @@
+#Challenge Exercise #5: Continuing with project #7, 
+#display the hello world message 10x
+
+#This programe demonstrates how the range function
+#can be used with a for loop.
+
+#Print the below message 10 times.
+for x in range(10):
+    print ('Hello world')
